b2f4675a9594158225c46bea64ffa183827fdf63
Finn Thye <piqueen@gmail.com>
2017-09-25 09:10:06 -0600

    Complete Tribonacci kata

--- 6-tribonacci.js
+++ 6-tribonacci.js
@@ -1,20 +1,30 @@
 function tribonacci(signature,n){
   /* 1. sum up signature
-  	 2. push it to the end of the new array
+     2. push it to the end of the new array
+      -- loop it --
      3. add up previous 3 indices
      4. push it to the end of the array
      5. continue until it pushes i < n */
-  let last3 = signature;
+  let sequence = signature;
   let nextValue = 0;
   
+  if (n === 0){
+    sequence = [];
+  };
   
-  nextValue = last3.reduce(function(sum,value) {
-  	return sum + value;
-  }, 0);
+  if (n === 1){
+    sequence = [signature[1]];
+  }
   
-  sequence.push(nextValue);
+  if (n === 2) {
+    sequence = [signature [2]];
+  }
+  
+  for (let i = 2; i < n - 1; i++){
+    let current = sequence[i];
+    nextValue = current + sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextValue)
+  };
   
   return sequence;
 }
-
-tribonacci([1,1,1],10);
