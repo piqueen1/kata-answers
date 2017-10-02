22705dae7b3c8a65a08faa13d5c6983dfaf1d225
Finn Thye <piqueen@gmail.com>
2017-10-02 06:08:28 -0600

    Finish 7-pull-words-together

--- 7-pull-words-together.js
+++ 7-pull-words-together.js
@@ -1,3 +1,19 @@
 function sentencify(words) {
-  // TODO: Write your solution here!
+  /*
+  1) capitalize first letter
+  2) add . to end
+  3) join ' '
+  */
+
+  //capitalize first letter
+  const newFirst = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+
+  //replace original first word with capitalized version
+  words[0] = newFirst;
+
+  //add . to end
+  words[words.length-1] += '.';
+
+  //join
+  return words.join(' ');
 }
