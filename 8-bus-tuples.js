9d2dfb3022bf5d96439def5b1b675b55b074f8f1
Finn Thye <piqueen@gmail.com>
2018-02-25 07:57:33 -0700

    Finish bus tuple kata

--- 8-bus-tuples.js
+++ 8-bus-tuples.js
@@ -1,5 +1,14 @@
 // https://www.codewars.com/kata/number-of-people-in-the-bus/train/javascript
 
+console.log('*****Program start*****');
 var number = function(busStops){
-  // Good Luck!
+  return busStops.reduce(function (accumulator, stop) {
+    accumulator = accumulator + stop[0];
+    accumulator = accumulator - stop[1];
+    return accumulator;
+  }, 0)
 }
+
+const answer = number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]);
+console.log('Answer: ', answer);
+console.log('******Program end******');
