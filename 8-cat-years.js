8bb497b234ab307e2831f58f05befbb07699d982
Finn Thye <piqueen@gmail.com>
2018-03-02 09:58:04 -0700

    Finish 8-cat-years.js

--- 8-cat-years.js
+++ 8-cat-years.js
@@ -1,5 +1,3 @@
-//https://www.codewars.com/kata/cat-years-dog-years/train/javascript
-
 console.log("****Start***")
 var humanYearsCatYearsDogYears = function(humanYears) {
   let answerArray = [];
@@ -7,7 +5,18 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   let dogYears = 0;
   
   if (humanYears >=1) {
+  	catYears = 15;
+    dogYears = 15;
+  }
   
+  if (humanYears >=2) {
+  	catYears = 24;
+    dogYears = 24;
+  }
+  
+  if (humanYears >=3) {
+  	catYears = 24+4*(humanYears-2);
+    dogYears = 24+5*(humanYears-2);
   }
   
   answerArray.push(humanYears);
@@ -16,15 +25,5 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   return answerArray;
 }
 
-console.log("answer ", humanYearsCatYearsDogYears(1))
+console.log("answer ", humanYearsCatYearsDogYears(10))
 console.log("****End***")
-
-/*Cat Years
-15 cat years for first year
-+9 cat years for second year
-+4 cat years for each year after that
-Dog Years
-15 dog years for first year
-+9 dog years for second year
-+5 dog years for each year after that
-*/
