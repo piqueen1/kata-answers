dd84509eeb210fc152f58c04002b824d8ad9647b
Finn Thye <piqueen@gmail.com>
2018-03-06 06:04:00 -0700

    Complete daily kata: 8-remove-character.js

---
+++ 8-remove-character.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/remove-first-and-last-character/train/javascript
+// Directions: remove the first and last character of a string
+function removeChar(str){
+    return str.slice(1,str.length - 1)
+};
+
+
