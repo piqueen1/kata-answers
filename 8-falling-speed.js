2d2202bc65efc0cfab119066273b06c1ef0a5bdb
Finn Thye <piqueen@gmail.com>
2018-03-06 20:26:02 -0700

    Complete 8-falling-speed.js

---
+++ 8-falling-speed.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/solutions/javascript/me/best_practice
+function sakuraFall(v) {
+    if(v<=0){
+      return 0;
+    }
+    return 400 / v;
+  }
