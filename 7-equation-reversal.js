52701934d41c742e67294f3574925fda4b8fe6d0
Finn Thye <piqueen@gmail.com>
2018-03-14 18:42:45 -0600

    Get a little closer to solving 7-equation-reversal.js

--- 7-equation-reversal.js
+++ 7-equation-reversal.js
@@ -1,30 +1,41 @@
-//https://www.codewars.com/kata/simple-equation-reversal/train/javascript
+console.log("****Start****");
+//https://www.codewars.com/kata/subtract-the-sum/train/javascript
 
 function solve(eq){
-    let reversed = eq.split('').reverse().join();
-    let reversedArray = Array.from(reversed);
-    let correctedArray = [];
+  let reversedArray = eq.split('').reverse();
+  let correctedArray = [];
+  
+  for (let i=0; i<reversedArray.length; i++) {
+    let character = reversedArray[i];
+    let numberHolder = [];
+    let isCharacterNumber = parseInt(character) >= 0;
+    let isNextCharacterNumber = parseInt(reversedArray[i+1]) >= 0;
     
-    for (let i=0; i<reversedArray.length; i++) {
-      let character = reversedArray[i];
-      let numberHolder = [];
-      
-      //if character is not an integer, add it to the end of an array
-      if (!Number.isInteger(character)){
-        correctedArray.push(character);
-      }
-      //others must be integers so add the to the start of a new array
-      numberHolder = numberHolder.unshift(character);
-      
-      /*if one number is an integer but the next is not, 
-      put the whole unreversed number back into the corrected array
-      and empty the numberHolder variable*/
-      
-      if (Number.isInteger(character) && !Number.isInteger(reversedArray[i+1])){
-          correctedArray.push(numberHolder);
-            numberHolder = [];
-      }
+    //if character is not an integer, add it to the end of an array
+    if (!isCharacterNumber){
+      correctedArray.push(character);
+      continue;
     }
     
-    return correctedArray.join();
+    console.log("numberBefore", numberHolder);
+    //others must be integers so add the to the start of a new array
+    numberHolder = numberHolder.unshift(character);
+    console.log("numberAfter", numberHolder);
+    
+    /*if one number is an integer but the next is not, 
+    put the whole unreversed number back into the corrected array
+    and empty the numberHolder variable*/
+    
+    if (!isNextCharacterNumber){
+		correctedArray = correctedArray.concat(numberHolder);
+      	numberHolder = [];
+      	continue;
+    }
   }
+  
+ // return correctedArray.join();
+  return correctedArray;
+}
+
+console.log(solve("100*b/y"));
+console.log("****End****");
