d3a17a67563ff367b248d0bd3a2cfddb8a29f5ab
Finn Thye <piqueen@gmail.com>
2018-07-18 21:34:30 -0600

    Start 8-exclusive-or.js

---
+++ 8-exclusive-or.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/exclusive-or-xor-logical-operator
+
+console.log('****START****');
+
+function xor(a, b) {
+  // TODO: Program Me
+}
+
+console.log('Answer: ', xor(tue, true));
+console.log('****END****');
