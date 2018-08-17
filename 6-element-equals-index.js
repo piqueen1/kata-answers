e5b3ab7b3a488690e58f4037a0b45bb0fa4c1e41
Finn Thye <piqueen@gmail.com>
2018-08-17 09:14:36 -0600

    Add solution to my slow implementation of 6-element-equals-index.js

--- 6-element-equals-index.js
+++ 6-element-equals-index.js
@@ -5,10 +5,40 @@ function indexEqualsValue(a) {
 
   for (let i = 0; i < a.length; i++) {
     if (i === a[i]) {
-      match = i;
+      return i;
+      break;
     }
   }
   return match;
 }
 console.log('Answer: ', indexEqualsValue([-8, 0, 2, 5])); // 2
 console.log('****END****');
+
+/* Result: my code took too long.  The correct
+answer is a binary search tree, like this:
+
+function indexEqualsValue(a) {
+  if (a[0] === 0)
+    return 0;
+  let result = -1;
+  let value;
+  let start = 0;
+  let length = a.length - 1;
+  let mid;
+  while (start != length) {
+    mid = Math.floor(( start + length ) / 2);
+    value = a[mid];
+    if(mid < value) {
+      length = mid;
+    }
+    else if( mid > value ) {
+      start = mid + 1;
+    }
+    else {
+      result = value;
+      length = mid;
+    }
+  }
+  return result;
+}
+*/
