101dcc3ef3e440c576ee4245e8b26709298a95c4
Finn Thye <piqueen@gmail.com>
2018-08-17 23:03:16 -0600

    Start 6-deadfish-parser.js

---
+++ 6-deadfish-parser.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/make-the-deadfish-swim
+
+function parse(sequence) {
+  let answerArray = [];
+  let total = 0;
+
+  let operations = {
+    i : () => {
+      total = total + 1;
+    },
+  }
+
+  return answerArray;
+}
