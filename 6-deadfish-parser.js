8be4044c30a2ba63853aaf15ba2d2d3f894015d3
Finn Thye <piqueen@gmail.com>
2018-08-18 08:12:43 -0600

    Finish 6-deadfish-parser.js

--- 6-deadfish-parser.js
+++ 6-deadfish-parser.js
@@ -1,14 +1,33 @@
+// Return the output array, and ignore all non-op characters
 // https://www.codewars.com/kata/make-the-deadfish-swim
 
 function parse(sequence) {
+  const sequenceArray = sequence.split('');
   let answerArray = [];
   let total = 0;
 
   let operations = {
-    i : () => {
+    'i': () => {
       total = total + 1;
     },
+    'd': () => {
+      total = total - 1;
+    },
+    's': () => {
+      total = total * total;
+    },
+    'o': () => {
+      answerArray.push(total);
+    },
+  }
+
+  for (let i = 0; i < sequenceArray.length; i++) {
+    let instruction = sequenceArray[i];
+    if (!/[idso]/.test(instruction)) { continue };
+    operations[instruction]();
   }
 
   return answerArray;
 }
+
+// parse("iiisdoso") // returns [8,64]
