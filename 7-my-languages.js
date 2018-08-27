e8331355e8f96e677f46757f1c9543c435d6712c
Finn Thye <piqueen@gmail.com>
2018-08-26 20:51:52 -0600

    Experiment with approaches to 7-my-languages.js

--- 7-my-languages.js
+++ 7-my-languages.js
@@ -3,22 +3,28 @@
 console.log('*** Start ***');
 
 function myLanguages(results) {
-  const languages = Object.keys(results);
-  const length = languages.length;
-
-  languages.sort();
-
-  const sortedScores = [];
-
-  for (let i=0; i<length; i++) {
-    //get current key
-    const current = [languages[i]];
-    console.log(current + ' : ' + languages[current]);
-    sortedScores.push({'score' : current, 'language' : languages[current]});
+  let languageKey = [];
+  for (let key in results) {
+    languageKey[results[key]] = key;
   }
-  return sortedScores;
+
+  return languageKey;
+  // const qualifyingLanguages = Object.keys(results).filter(key => results[key] >= 60);
+  // let orderedArray = [];
+
+  // for (let i=0; i<qualifyingLanguages.length; i++) {
+  //   const currentLanguage= qualifyingLanguages[i];
+  //   const currentScore = results.currentLanguage;
+
+  //   //Make array of key:value where key is score and value is language
+  //   orderedArray.push({ currentScore : currentLanguage });
+
+  //   //Order that array by score
+  //   //Pull off language values into an array preserving the order
+  // }
+
+  // return orderedArray;
 }
 
 console.log('The answer is... ', myLanguages({ "Java": 10, "Ruby": 80, "Python": 65 }));
 console.log('*** End ***');
-
