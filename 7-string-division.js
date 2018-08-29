8581cd0a17ab82de5f5cab9e714088b303c66aae
Finn Thye <piqueen@gmail.com>
2018-08-28 20:17:58 -0600

    Create 7-string-division.js

---
+++ 7-string-division.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/simple-string-division/train/javascript
+
+console.log('***start***');
+
+function solve(str, k) {
+  return str;
+}
+console.log(solve('1234', 1))
+console.log('*** end ***');
