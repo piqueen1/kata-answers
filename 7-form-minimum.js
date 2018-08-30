246e492b4fc2d5f75a9cd080d713cefb3f8f538d
Finn Thye <piqueen@gmail.com>
2018-08-30 16:44:21 -0600

    Finish 7-form-minimum.js

--- 7-form-minimum.js
+++ 7-form-minimum.js
@@ -1,1 +1,13 @@
 // https://www.codewars.com/kata/form-the-minimum/train/javascript
+
+console.log('***start***');
+
+function minValue(values) {
+  let uniqueSorted = parseInt([...new Set(values)].sort((a,b) => a - b).join(''));
+
+
+return uniqueSorted;
+}
+
+console.log(minValue([1, 3, 1])); // 13;
+console.log('*** end ***');
