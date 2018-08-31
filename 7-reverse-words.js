cbbe25b69c63d0970fd6d1ce6ead8ef32b32df1f
Finn Thye <piqueen@gmail.com>
2018-08-31 10:56:29 -0600

    Create 7-reverse-words.js

---
+++ 7-reverse-words.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/reverse-words/train/javascript
+
+console.log('***start***');
+
+function reverseWords(str) {
+  // Go for it
+}
+console.log(minValue([1, 3, 1])); // 13;
+console.log('*** end ***');
