0c8f45bf9a2c642052ffc916a88b16fb6b4ec084
Finn Thye <piqueen@gmail.com>
2018-09-04 06:08:03 -0600

    Create 7-remove-duplicate.js

---
+++ 7-remove-duplicate.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/remove-duplicate-words/train/javascript
+
+console.log('***start***');
+
+function removeDuplicateWords(s) {
+  // your perfect code...
+}
+
+console.log(oddCount(7)); //3
+console.log('*** end ***');
