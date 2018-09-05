eeb8087663668959ea335e6b6dd502cb208d3685
Finn Thye <piqueen@gmail.com>
2018-09-05 11:10:32 -0600

    Create 8-rearrange-array.js

---
+++ 8-rearrange-array.js
@@ -0,0 +1,8 @@
+console.log('***start***');
+
+function fixTheMeerkat(arr) {
+  console.log(arr);
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"])); // ["head", "body", "tail"]); // 'alpha beta gamma delta');
+console.log('*** end ***');
