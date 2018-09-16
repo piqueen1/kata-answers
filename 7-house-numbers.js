c7518723fe8c301501fddc69a70450c14f1b2070
Finn Thye <piqueen@gmail.com>
2018-09-15 21:11:35 -0600

    Create and add template to 7-house-numbrs.js

---
+++ 7-house-numbers.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/simple-fun-number-37-house-numbers-sum/train/javascript
+
+console.log('***start***');
+
+function houseNumbersSum(inputArray) {
+  
+}
+
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2])); // 11
+console.log('*** end ***');
