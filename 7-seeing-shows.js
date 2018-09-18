3de63f0beb0ecc18c84fe0d1e01d1034174767ce
Finn Thye <piqueen@gmail.com>
2018-09-18 06:16:46 -0600

    Create 7-seeing-shows.js

---
+++ 7-seeing-shows.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/seeing-shows-at-the-edinburgh-fringe/train/javascript
+
+console.log('***start***');
+
+function chooseShows(showList) {
+  return showList;
+}
+
+console.log(chooseShows([
+  {
+    name: "Little Death Club",
+    time: 8,
+    length: 2,
+    price: 10,
+  },
+  {
+    name: "Courtney Act",
+    time: 6,
+    length: 1,
+    price: 10,
+  },
+  {
+    name: "Reversible",
+    time: 7,
+    length: 1,
+    price: 12,
+  }
+])); // [['Courtney Act', 'Little Death Club'], 20]
+console.log('*** end ***');
