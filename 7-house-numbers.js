e6b2d18a27cd47cfc00d614449f6558ea45289d0
Finn Thye <piqueen@gmail.com>
2018-09-18 05:07:21 -0600

    Finish 7-house-numbers.js

--- 7-house-numbers.js
+++ 7-house-numbers.js
@@ -3,7 +3,18 @@
 console.log('***start***');
 
 function houseNumbersSum(inputArray) {
-  
+  let sum = 0;
+
+  for (let i=0; i<inputArray.length; i++) {
+    sum = sum + inputArray[i];
+
+    if (inputArray[i] === 0) {
+      return sum;
+    }
+    continue;
+  }
+
+  return sum;
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2])); // 11
