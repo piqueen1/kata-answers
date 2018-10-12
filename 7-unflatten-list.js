97e348804d02c7e6c422d064fb51e2f0239e560e
Finn Thye <piqueen@gmail.com>
2018-10-12 06:07:20 -0600

    Start 7-unflatten-list.js

---
+++ 7-unflatten-list.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/unflatten-a-list-easy/train/javascript
+
+console.log('***start***');
+
+function unflatten(flatArray) {
+
+}
+
+console.log(calculate("1plus2plus3plus4")); // '10'
+console.log('*** end ***');
