833f00c63f90e5f4c4a25429cfeb36f2c5d31cb6
Finn Thye <piqueen@gmail.com>
2018-10-15 06:13:06 -0600

    Create 5-sort-deep-alphabetic.js

---
+++ 5-sort-deep-alphabetic.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/sort-one-three-two/train/javascript
+
+console.log('***start***');
+
+function sortByName(ary) {
+  // Your code here
+  return ary;
+}
+
+console.log(sortByName([1, 2, 3, 4])); // [4, 1, 3, 2]
+console.log('*** end ***');
