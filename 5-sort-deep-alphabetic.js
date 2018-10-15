d1844e73ebb541be842d590b5310cf91166cbf5c
Finn Thye <piqueen@gmail.com>
2018-10-15 08:49:58 -0600

    Finished converting digit array to string array

--- 5-sort-deep-alphabetic.js
+++ 5-sort-deep-alphabetic.js
@@ -3,8 +3,11 @@
 console.log('***start***');
 
 function sortByName(ary) {
-  // Your code here
-  return ary;
+  const table = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  // map digits to strings
+  const strings = ary.map(x => table[x]);
+
+  return strings;
 }
 
 console.log(sortByName([1, 2, 3, 4])); // [4, 1, 3, 2]
