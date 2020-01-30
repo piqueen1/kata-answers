f1445f28a63685b4fb66a4d5772712fb1e34de48
Finn Thye <piqueen@gmail.com>
2020-01-30 10:39:12 -0700

    Solved first of logged questions, finished 8-ES6-classes-people.js

---
+++ 8-ES6-classes-people.js
@@ -0,0 +1,20 @@
+kata: https://www.codewars.com/kata/fun-with-es6-classes-number-1-people-people-people/train/javascript
+
+class Person {
+
+  constructor (firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  sayFullName() {
+    return this.firstName + " " + this.lastName;
+  };
+
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  };
+
+}
