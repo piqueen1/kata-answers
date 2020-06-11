a202363e06d545c2b5d310e2c8e51b14e54b0e7d
Finn Thye <piqueen@gmail.com>
2020-06-11 16:29:12 -0600

    Finish 6-adjacent-words.js

--- 6-adjacent-words.js
+++ 6-adjacent-words.js
@@ -2,8 +2,18 @@
 
 function countAdjacentPairs(searchString) {
   if (searchString.length === 0) return 0;
+
+  const searchArray = searchString.toLowerCase().split(' ');
+
+  let pairs = 0;
   
-  return searchString;
+  for (let i=0; i<searchArray.length; i++) {
+    if (searchArray[i-1] && (searchArray[i-1] === searchArray[i])) {
+      if ((searchArray[i-2] && (searchArray[i-2] === searchArray[i]))) {continue} else { pairs += 1 };
+    } else {continue};
+  }
+
+  return pairs;
 }
 
 console.log(countAdjacentPairs('orange Orange kiwi pineapple apple')) // 1
