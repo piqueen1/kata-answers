66d4c2fe6d715d14622f1200465f36b58db98d9a
Finn Thye <piqueen@gmail.com>
2020-06-11 15:32:12 -0600

    Finish 7-nth-poower.js

--- 7-nth-power.js
+++ 7-nth-power.js
@@ -1,8 +1,10 @@
 //https://www.codewars.com/kata/58aed2cafab8faca1d000e20/train/javascript
 
 function modifiedSum(array, number) {
-  
+  const raised = array.reduce((a,b) => a + (b**number), 0);
+  const arrSum = array.reduce((a,b) => a + b, 0);
 
+  return raised - arrSum;
 }
 
-console.log(modifiedSum([1, 2, 3], 3) // 30
+console.log(modifiedSum([1, 2, 3], 3)); // 30
