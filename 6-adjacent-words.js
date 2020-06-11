e27751a780c23fe6252ef73ca2e7fb4a4944da78
Finn Thye <piqueen@gmail.com>
2020-06-11 15:51:51 -0600

    Create 6-adjacent-words.js

---
+++ 6-adjacent-words.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5245a9138ca049e9a10007b8/train/javascript
+
+function countAdjacentPairs(searchString) {
+  if (searchString.length === 0) return 0;
+  
+  return searchString;
+}
+
+console.log(countAdjacentPairs('orange Orange kiwi pineapple apple')) // 1
