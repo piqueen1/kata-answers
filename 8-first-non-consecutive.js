7e361fb7e6a92417c44e98d18b7b164b5e550263
Finn Thye <piqueen@gmail.com>
2020-06-14 08:32:19 -0600

    Solved 8-first-non-consecutive

--- 8-first-non-consecutive.js
+++ 8-first-non-consecutive.js
@@ -1,22 +1,18 @@
 // https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
 
 function firstNonConsecutive (arr) {
-  let counter = 0;
-  console.log('array: ', arr);
+  let counter;
 
   for (let i=0;i<arr.length;i++) {
-    if (i === arr.length-1) {
-      counter = null;
-      break;
-    } else if ( arr[i-1] && (arr[i] !== (arr[i-1] + 1))) {
+    if ( arr[i-1] && (arr[i] !== (arr[i-1] + 1))) {
       counter = arr[i];
+      break;
+    } else if (i === arr.length-1) {
+      counter = null;
       break;
     } else {
       continue;
     };
   }
-
-  return counter;
-}
 
 console.log(firstNonConsecutive([1,2,3,4,6,7,8])); // 6
