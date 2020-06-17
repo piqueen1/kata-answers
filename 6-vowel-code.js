07e8d39beef01ed493b9be161f611e14432374a0
Finn Thye <piqueen@gmail.com>
2020-06-16 20:53:28 -0600

    Finish 6-vowel-code.js

--- 6-vowel-code.js
+++ 6-vowel-code.js
@@ -1,20 +1,44 @@
 // https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript
 
 function encode(string) {
+  const characterArray = string.split('');
+  const code = {
+    'a': '1',
+    'e': '2',
+    'i': '3',
+    'o': '4',
+    'u': '5',
+  }
+  const newArray = characterArray.map((character) => {
+    if (character in code) {
+      return code[character];
+    } else {
+      return character;
+    }
+  });
 
-  // 1. split string into array
-  // 2. iterate -- map
-  //      in iteration, 
-  //      if the character is a key of an object,
-            // return that value
-          // otherwise the original unchanged
-  //  3. join character array back into string
-
-  return
+  return newArray.join('');
 }
 
 function decode(string) {
-  return
+  const characterArray = string.split('');
+  const code = {
+    '1': 'a',
+    '2': 'e',
+    '3': 'i',
+    '4': 'o',
+    '5': 'u',
+  }
+  const newArray = characterArray.map((character) => {
+    if (character in code) {
+      return code[character];
+    } else {
+      return character;
+    }
+  });
+
+  return newArray.join('');
 }
+
 
 console.log(encode('hello')); // 'h2ll4'
