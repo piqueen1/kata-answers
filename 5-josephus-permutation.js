b18642087e1091105be5e0ca2152618d6e92714c
Finn Thye <piqueen@gmail.com>
2020-06-17 13:34:07 -0600

    Create 5-josephus-permutation.js

---
+++ 5-josephus-permutation.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5550d638a99ddb113e0000a2
+// https://en.wikipedia.org/wiki/Josephus_problem
+
+function josephus(items,k){
+  return items.in
+}
+
+console.log(josephus([1,2,3,4],2))
