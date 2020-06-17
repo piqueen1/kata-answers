b07574d93751f3852438e8d25399fe47877fa71b
Finn Thye <piqueen@gmail.com>
2020-06-17 16:56:21 -0600

    Pseuocode josephus

--- 5-josephus-permutation.js
+++ 5-josephus-permutation.js
@@ -2,7 +2,23 @@
 // https://en.wikipedia.org/wiki/Josephus_problem
 
 function josephus(items,k){
-  return items.in
+  if (items.length % 2 === 0) {
+
+    for (let i=1;i<items.length;i++) {
+      //  if (i is a multiple of k, if k%i === 0) {
+        if (k%i == 0) // CONTINUE HERE
+      //    slice out that index's value
+      //    glue that value onto the end of the result array
+      //  if (i = items.length - 1) {
+      //    i = 0
+      //   }
+    }
+
+  } else {
+
+  }
+
+  return items.pop;
 }
 
 console.log(josephus([1,2,3,4],2))
