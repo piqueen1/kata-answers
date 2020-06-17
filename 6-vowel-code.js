0ad1516ed58ad77adb2a1577ae40cc69e0b004c9
Finn Thye <piqueen@gmail.com>
2020-06-16 18:25:36 -0600

    Create 6-vowel-code

---
+++ 6-vowel-code.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript
+
+function encode(string) {
+
+  // 1. split string into array
+  // 2. iterate -- map
+  //      in iteration, 
+  //      if the character is a key of an object,
+            // return that value
+          // otherwise the original unchanged
+  //  3. join character array back into string
+
+  return
+}
+
+function decode(string) {
+  return
+}
+
+console.log(encode('hello')); // 'h2ll4'
