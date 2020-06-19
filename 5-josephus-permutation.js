1b78bf286bb5b0cbc318e74a93d471a41757e444
Finn Thye <piqueen@gmail.com>
2020-06-19 13:51:00 -0600

    Working on Josephus

--- 5-josephus-permutation.js
+++ 5-josephus-permutation.js
@@ -1,19 +1,20 @@
 // https://www.codewars.com/kata/5550d638a99ddb113e0000a2
 // https://en.wikipedia.org/wiki/Josephus_problem
+// NEXT: figure out logic of for loop
 
 function josephus(items,k){
+  let answerArray = [];
   if (items.length % 2 === 0) {
 
     for (let i=1;i<items.length;i++) {
       //  if (i is a multiple of k, if k%i === 0) {
-        if (k%i == 0) // CONTINUE HERE
+        if (k%i === 0) // CONTINUE HERE
       //    slice out that index's value
       //    glue that value onto the end of the result array
       //  if (i = items.length - 1) {
       //    i = 0
       //   }
-    }
-
+    };
   } else {
 
   }
