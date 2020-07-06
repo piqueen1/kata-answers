2b432c4f2bc5220cbc327ee458af5a1a7d7b85fb
Finn Thye <piqueen@gmail.com>
2020-07-06 10:43:13 -0600

    Pseudocode and outline js for 6-find-odd.js

--- 6-find-odd.js
+++ 6-find-odd.js
@@ -1,6 +1,21 @@
 // https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
 
 function findOdd(array) {
-  
-  return 0;
+  let pairArray = array.map(element => {
+    const original = element;
+    let originalCounter = 1;
+
+    for (let i=0; i<array.length; i++){
+      const current
+    }
+
+    return { originalCounter : original}
+  });
+
+  return the value of the max key
 }
+
+for each element look at every element in the array
+compare to index 0.  If element matches index, object gets {element: elementCounter}
+return that key:value pair into a big object
+return the element value of the largest elementCounter
