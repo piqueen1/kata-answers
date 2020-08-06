c89b63d7c2f38f8cce904cdd58df9e9cc1581bea
Finn Thye <piqueen@gmail.com>
2020-08-05 21:02:40 -0600

    Create 8-fake-binary.js

--- 8-check-exam.js
+++ 8-check-exam.js
@@ -1,30 +1,25 @@
 https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/javascript
 
 function checkExam(array1, array2) {
-  return array1.map((letter1) => {
+  const scoresArray = array1.map((letter1) => {
     let score = 0;
 
     for (let i=0;i<array1;i++) {
       const letter2 = array2[i];
 
       switch (letter2) {
-        case (letter1 === letter2) {
+        case (letter1 === ''):
+          break;
+        case (letter1 === letter2):
           score += 4;
           break;
-          };
-
-          case(letter1 !== letter2){
+        case (letter1 !== letter2):
             score -= 1;
             break;
-          }
-
-          default {
-            break;
-          }
-        }
+        default
       }
-
-      return score;
+      
+    return score;
     }
   });
 }
---
+++ 8-fake-binary.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
+function fakeBin(x){
+  
+}
+
+console.log(fakeBin('45385593107843568'))// '01011110001100111')
