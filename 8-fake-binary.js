3876bb471106cf631f60593ea4e66066f43ee030
Finn Thye <piqueen@gmail.com>
2020-08-05 22:37:09 -0600

    Solve 8-fake-binary.js

--- 8-fake-binary.js
+++ 8-fake-binary.js
@@ -1,7 +1,8 @@
 //https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
 
-function fakeBin(x){
-  
+function fakeBin(numString){
+  const intArray = numString.split('').map( x => parseInt(x,10) );
+  return intArray.map(x => x<5 ? '0' : '1').join('');
 }
 
 console.log(fakeBin('45385593107843568'))// '01011110001100111')
