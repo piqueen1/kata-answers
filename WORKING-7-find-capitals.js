bb04cec5a0af9ecc725fa02062679fb3532aeebd
Finn Thye <piqueen@gmail.com>
2020-08-09 15:10:56 -0600

    Progress on WORKING-7-find-capitals.js

--- WORKING-7-find-capitals.js
+++ WORKING-7-find-capitals.js
@@ -1,7 +1,14 @@
 function capital(capitals){
   let phraseArray = [];
+  let properties = Object.getOwnPropertyNames(capitals);
+  //NEXT: reference each value, not each key
   
   for (let i=0;i<capitals.length;i++) {
-    const key = 
-    phraseArray.push(`The capital of ${capitals[i]} is ca`)
-  }
+    const capitol = capitals[i].capital;
+    phraseArray.push(`The capitol of this entity is ${capitol}`);
+  };
+  
+  return properties;
+}
+
+console.log(capital( [{state: 'Maine', capital: 'Augusta'}][0])) //, "The capital of Maine is Augusta")
