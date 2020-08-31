d80b6deb5650be4694d2396ce70dee7d45e42b31
Finn Thye <piqueen@gmail.com>
2020-08-31 14:11:36 -0600

    Created and almost solved 8-parse-float.js

---
+++ 8-parse-float.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/57a386117cb1f31890000039/train/javascript
+
+function parseF(s) {
+  console.log(s)
+  
+  if (isNaN(s)) {
+    return null 
+    } else if (s === true || false) {
+    return null
+    } else {
+    return parseFloat(s)
+  }
+}
+
+console.log(parseF("1")) //, 1.0);)
