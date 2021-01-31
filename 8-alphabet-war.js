3f6ccb01668830fcd1a8289b6da6732ec17685d8
Finn Thye <piqueen@gmail.com>
2021-01-31 09:57:05 -0700

    Won kata 8-alphabet-war.js

--- 8-alphabet-war.js
+++ 8-alphabet-war.js
@@ -2,13 +2,28 @@
 
 function alphabetWar(fight)
 {
-   
-  return "Let's fight again!";
+  const values = {
+    w: 4,
+    p: 3,
+    b: 2,
+    s: 1,
+    m: -4,
+    q: -3,
+    d: -2,
+    z: -1
+   }
+   let score = 0;
+   let fightArray = fight.split('')
 
-   w - 4
-   p - 3
-   b - 2
-   s - 1
+   fightArray.forEach(c => {
+     if (Object.keys(values).includes(c)) {
+       score += values[c]
+     }
+   })
+
+   return score > 0 ? "Left side wins!" :
+    score < 0 ? "Right side wins!" :
+    "Let's fight again!";
 }
 
-alphabetWar("z") //, "Right side wins!"
+console.log(alphabetWar("z")) //, "Right side wins!"
