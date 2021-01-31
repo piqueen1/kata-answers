f18d11ae0ce4335ea4771fbed9cbf15caf081c23
Finn Thye <piqueen@gmail.com>
2021-01-31 09:38:32 -0700

    Set up 8-alphabet-war.js

---
+++ 8-alphabet-war.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/59377c53e66267c8f6000027/train/javascript
+
+function alphabetWar(fight)
+{
+   
+  return "Let's fight again!";
+
+   w - 4
+   p - 3
+   b - 2
+   s - 1
+}
+
+alphabetWar("z") //, "Right side wins!"
