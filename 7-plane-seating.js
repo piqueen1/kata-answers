e61b19d7d17ddbe5f33981cd838921fe889b7509
Finn Thye <piqueen@gmail.com>
2021-02-02 08:42:05 -0700

    Set up 7-plane-seating.js

---
+++ 7-plane-seating.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/57e8f757085f7c7d6300009a/train/javascript
+
+function planeSeat(a){
+  const letters = {
+    A: "Left",
+    B: "Left",
+    C: "Left",
+    D: "Middle",
+  }
+
+
+}
