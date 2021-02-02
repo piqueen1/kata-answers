2fd0e1e12fac917731c01d538e14ee4eb3843854
Finn Thye <piqueen@gmail.com>
2021-02-02 09:24:10 -0700

    Solved 7-plane-seating.js

--- 7-plane-seating.js
+++ 7-plane-seating.js
@@ -1,4 +1,6 @@
 //https://www.codewars.com/kata/57e8f757085f7c7d6300009a/train/javascript
+let side = ""
+let section = ""
 
 function planeSeat(a){
   const letterSide = {
@@ -14,16 +16,14 @@ function planeSeat(a){
   }
 
   function findSide(letter) {
-    let side = ""
-    if (side.keys.includes(letter)) {
-      side = letterSide.letter
+    if (Object.keys(letterSide).includes(letter)) {
+      side = letterSide[letter]
     } else {
       side = "No Seat!!"
     }
   }
 
   function findSection(num) {
-    let section = ""
     if (num >= 1 && num <= 20) {
       section = "Front"
     } else if (num >= 21 && num <= 40) {
@@ -35,11 +35,17 @@ function planeSeat(a){
     }
   }
 
-  const assignedArray = a.split()''
-  let assignedSide = a.split().slice(-1)
-  let assignedSection = a
+  let assignedSide = a.slice(-1)
+  let assignedSection = parseInt(a.substring(0, a.length - 1));
 
-  console.log(assignedSide, assignedSection)
+  findSide(assignedSide)
+  findSection(assignedSection)
+
+  if (section === "No Seat!!" || side === "No Seat!!") {
+    return "No Seat!!"
+  } else {
+    return `${section}-${side}`
+  }
 }
 
-planeSeat("27b")
+console.log(planeSeat("27B"))
