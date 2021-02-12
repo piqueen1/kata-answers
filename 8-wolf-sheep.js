5a379f9d21ec60baf97d1f4074ae3a957556ae7f
Finn Thye <piqueen@gmail.com>
2021-02-12 12:56:47 -0700

    Solved 8-wolf-sheep.js

--- 8-wolf-sheep.js
+++ 8-wolf-sheep.js
@@ -1,7 +1,16 @@
 // https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
 
 function warnTheSheep(queue) {
+  let wolf = 0
+  
+  for (let i=0;i<queue.length;i++) {
+    if (queue[i]==="wolf")
+      wolf = queue.length - i
+  }
+  
+  let sheep = wolf - 1
 
+  return sheep === 0 ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${sheep}! You are about to be eaten by a wolf!`
 }
 
 console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])) //, "Oi! Sheep number 2! You are about to be eaten by a wolf!");)
