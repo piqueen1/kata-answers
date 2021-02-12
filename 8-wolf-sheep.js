e76430aacec2d51b65c4c9a928d9590d82c667bb
Finn Thye <piqueen@gmail.com>
2021-02-12 12:40:00 -0700

    Prepare 8-wolf-sheep.js

---
+++ 8-wolf-sheep.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+
+function warnTheSheep(queue) {
+
+}
+
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])) //, "Oi! Sheep number 2! You are about to be eaten by a wolf!");)
