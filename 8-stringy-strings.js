70126ff31e2cb3e0419908cf8efd65afa6236dd9
Finn Thye <piqueen@gmail.com>
2021-02-15 20:41:14 -1000

    Solved 8-stringy-strings.js

--- 8-stringy-strings.js
+++ 8-stringy-strings.js
@@ -1,7 +1,17 @@
 // https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/javascript
 
 function stringy(size) {
-  // your code here   
+  let sequence = '1'
+
+  for (let i=0; i<size - 1; i++) {
+    if (sequence.length % 2 != 0) {
+      sequence += '0'
+    } else {
+      sequence += '1'
+    }
+  }
+
+  return sequence
 }
 
-console.log(stringy(3)) // '101'
+console.log(stringy(5)) // '101'
