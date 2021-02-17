2a21c41a414d2098cf9acf0ac50a1632a003a7e5
Finn Thye <piqueen@gmail.com>
2021-02-16 17:23:05 -1000

    Solved 8-spoonerize-me.js

--- 8-spoonerize-me.js
+++ 8-spoonerize-me.js
@@ -1,7 +1,11 @@
 // https://www.codewars.com/kata/56b8903933dbe5831e000c76/train/javascript
 
 function spoonerize(words) {
-  //...aaaaand SPOONERIZE!
+  const wordArray = words.split(' ')
+  const firstLetter1 = wordArray[1].charAt(0) + wordArray[0].slice(1)
+  const firstLetter2 = wordArray[0].charAt(0) + wordArray[1].slice(1)
+
+  return firstLetter1 + ' ' + firstLetter2
 }
 
 console.log(spoonerize("nit picking")) // "pit nicking"
