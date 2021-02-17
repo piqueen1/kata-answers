0c2a6b87e6f138113765f8a8ad821a2f535510ef
Finn Thye <piqueen@gmail.com>
2021-02-17 09:50:35 -1000

    Set up 6-penguin-olympics.js

---
+++ 6-penguin-olympics.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/6022c97dac16b0001c0e7ccc/train/javascript
+
+function calculateWinners(snapshot, penguins) {
+  // TODO: solve kata
+};
+
+const snapshot = `|----p---~---------|
+|----p---~~--------|
+|----p---~~~-------|`;
+
+const penguins = ["Derek", "Francis", "Bob"];
+
+console.log(calculateWinners(snapshot, penguins)) // "GOLD: Derek, SILVER: Francis, BRONZE: Bob";
