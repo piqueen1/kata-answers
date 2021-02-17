5da79155b8d5666bc795ccaf2e67946d4be481ba
Finn Thye <piqueen@gmail.com>
2021-02-16 14:07:27 -1000

    Set up 8-spoonerize-me.js

---
+++ 8-spoonerize-me.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56b8903933dbe5831e000c76/train/javascript
+
+function spoonerize(words) {
+  //...aaaaand SPOONERIZE!
+}
+
+console.log(spoonerize("nit picking")) // "pit nicking"
