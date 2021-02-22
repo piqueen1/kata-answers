0a956232fac99d9d3e17ada6dcbcbc91680d53e5
Finn Thye <piqueen@gmail.com>
2021-02-22 16:55:18 -0700

    Solved 8-get-bonus.js

--- 8-get-bonus.js
+++ 8-get-bonus.js
@@ -1,7 +1,9 @@
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
 
 function bonusTime(salary, bonus) {
-  // your code here
-  }
+  const amount = bonus ? salary*10 : salary
+  
+  return `£${amount}`
+}
 
   console.log(bonusTime(10000, true)) // '£100000'
