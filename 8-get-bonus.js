159d223818e2e10ee1739d4ab0bbe1c042949f5c
Finn Thye <piqueen@gmail.com>
2021-02-21 19:15:28 -0700

    Set up 8-get-bonus.js

---
+++ 8-get-bonus.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+  // your code here
+  }
+
+  console.log(bonusTime(10000, true)) // '£100000'
